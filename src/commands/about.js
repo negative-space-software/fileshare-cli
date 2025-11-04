c86d8e901fd6fdb9ece7f8a357f3940302430846
nicho <nicholastroiano123@gmail.com>
2025-11-04 14:22:29 -0500

    Add Apache 2.0 license to project
    - Add LICENSE file with Apache 2.0 license text
    - Update package.json to specify Apache-2.0 license
    - Add license headers to all source files
    - Update about command to display license information

--- src/commands/about.js
+++ src/commands/about.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 Negative Space Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const chalk = require('chalk');
 const asciify = require('asciify-image');
 const path = require('path');
@@ -30,6 +46,7 @@ async function aboutCommand() {
     console.log('');
     console.log('  ' + chalk.bold('Version:  ') + chalk.green('v1.0.0'));
     console.log('  ' + chalk.bold('Author:   ') + chalk.yellow('Negative Space Software'));
+    console.log('  ' + chalk.bold('License:  ') + chalk.magenta('Apache License 2.0'));
     console.log('');
     console.log('  ' + chalk.bold('Server:   ') + chalk.blue('https://fileshare.ct-42210.com'));
     console.log('');
